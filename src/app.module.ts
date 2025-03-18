dcdb6378656609cba30a049c66dfc92ef2ad07b3
biglol <nba2005kings@naver.com>
2025-03-19 00:51:28 +0900

    doing auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { ProfileModel } from './posts/entities/profile.entity';
 import { TagModel } from './posts/entities/tag.entity';
 import { UsersModule } from './users/users.module';
 import { UsersModel } from './users/entities/users.entity';
+import { AuthModule } from './auth/auth.module';
 // module.ts 같은 경우는 우리가 컨트롤러와 서비스를 포함한 다른 프로바이더들을 관리. 의존성들을 관리하게 되는 파일
 @Module({
   imports: [
@@ -34,6 +35,7 @@ import { UsersModel } from './users/entities/users.entity';
       synchronize: true, // nestjs에서 작성하는 typeorm코드와 데이터베이스의 동기화를 자동으로 맞춤
     }),
     UsersModule,
+    AuthModule,
   ], // 다른 모듈을 불러올 때 사용. cli를 이용했으니 자동으로 생성됨
   controllers: [AppController],
   providers: [AppService],
